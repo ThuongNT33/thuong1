ceed0b42c2bd68a7479bf25f18a09233c979ea15
ThuongNT33 <66453501+ThuongNT33@users.noreply.github.com>
2020-06-19 09:17:51 +0700

    Add files via upload

---
+++ BCSPKoBanĐuocTheoQui.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace QuanLyCuaHangSach
+{
+    partial class FrmBCSPKoBánĐươcTheoQuí
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dtpThangKoBánĐc = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rpwBCSPKoBánĐược = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.dtpThangKoBánĐc);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(735, 103);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Điều kiện tạo báo cáo";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(306, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Tạo báo cáo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dtpThangKoBánĐc
+            // 
+            this.dtpThangKoBánĐc.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpThangKoBánĐc.Location = new System.Drawing.Point(152, 25);
+            this.dtpThangKoBánĐc.Name = "dtpThangKoBánĐc";
+            this.dtpThangKoBánĐc.Size = new System.Drawing.Size(88, 20);
+            this.dtpThangKoBánĐc.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tháng ko bán đc sp:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rpwBCSPKoBánĐược);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 103);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(735, 330);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Kết quả báo cáo";
+            // 
+            // rpwBCSPKoBánĐược
+            // 
+            this.rpwBCSPKoBánĐược.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rpwBCSPKoBánĐược.Location = new System.Drawing.Point(3, 16);
+            this.rpwBCSPKoBánĐược.Name = "rpwBCSPKoBánĐược";
+            this.rpwBCSPKoBánĐược.Size = new System.Drawing.Size(729, 311);
+            this.rpwBCSPKoBánĐược.TabIndex = 0;
+            // 
+            // FrmBCSPKoBánĐươcTheoQuí
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 433);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FrmBCSPKoBánĐươcTheoQuí";
+            this.Text = "Báo cáo sản phẩm ko bán được theo quí";
+            this.Load += new System.EventHandler(this.FrmBCSPKoBánĐươcTheoQuí_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dtpThangKoBánĐc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private Microsoft.Reporting.WinForms.ReportViewer rpwBCSPKoBánĐược;
+    }
+}
